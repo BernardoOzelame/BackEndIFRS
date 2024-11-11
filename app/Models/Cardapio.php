fc042526fcf153eeada6b881c0e6acfdfcf6d273
eliseutrembala <eliseuc715@gmail.com>
2024-11-11 14:59:16 -0300

    Models v1

--- app/Models/Cardapio.php
+++ app/Models/Cardapio.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Usuario extends Model
+class Cardapio extends Model
 {
     protected $fillable = [
-        'login',
-        'senha',
-        'tipo'
+        'data',
+        'tipoRefeicao',
+        'nutricionistaId'
     ];
 }
